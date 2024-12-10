e7854019ea46bb254fa6dfd9eac149038bf61bb5
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-12-10 22:53:07 +1300

    acme.js
    use substring instead of substr in hexToBytes

--- acme.js
+++ acme.js
@@ -467,7 +467,7 @@ export function base64urlEncode(input) {
 export function hexToBytes(hex) {
     const bytes = new Uint8Array(hex.length / 2);
     for (let i = 0; i < hex.length; i += 2) {
-        bytes[i / 2] = parseInt(hex.substr(i, 2), 16);
+        bytes[i / 2] = parseInt(hex.substring(i, i + 2), 16);
     }
     return bytes;
 }
